f0b4e1449b6b759295d7f7b70ff09f4b753cc614
FernandoHdez67 <20200767@uthh.edu.mx>
2023-10-10 09:44:11 -0600

    primer deploy

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue " />
 </template>
 
 <style scoped>
@@ -28,3 +28,4 @@ import HelloWorld from './components/HelloWorld.vue'
   filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
+
